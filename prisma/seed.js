9460c211c0f831a01ebbd4a92c6ff052e1dcb80c
hbagus.dev <bagushartiansyah07@gmail.com>
2025-11-17 13:34:42 -0800

    v18

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,96 +1,96 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';
-
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
 
 const prisma = new PrismaClient();
 
 async function main() {
   const hashedPassword = await bcrypt.hash('admin123', 10);
 
-  await prisma.user.createMany({
-    data: [
-      {
-      name: 'Admin',
-      email: 'admin@bpkad.com',
-      pass: hashedPassword,
-      level:1
-    },{
-      name: 'Sekretariat',
-      email: 'sekretariat@bpkad.com',
-      pass: hashedPassword,
-      level:1
-    },{
-      name: 'Anggaran',
-      email: 'anggaran@bpkad.com',
-      pass: hashedPassword,
-      level:1
-    },{
-      name: 'Perbendaharaan',
-      email: 'perbendaharaan@bpkad.com',
-      pass: hashedPassword,
-      level:1
-    },{
-      name: 'Akuntansi',
-      email: 'akuntansi@bpkad.com',
-      pass: hashedPassword,
-      level:1
-    },{
-      name: 'Aset',
-      email: 'aset@bpkad.com',
-      pass: hashedPassword,
-      level:1
-    },
-    ]
-  });
+  // await prisma.user.createMany({
+  //   data: [
+  //     { name: 'Admin', email: 'admin@bpkad.com', pass: hashedPassword, level: 1 },
+  //     { name: 'Sekretariat', email: 'sekretariat@bpkad.com', pass: hashedPassword, level: 1 },
+  //     { name: 'Anggaran', email: 'anggaran@bpkad.com', pass: hashedPassword, level: 1 },
+  //     { name: 'Perbendaharaan', email: 'perbendaharaan@bpkad.com', pass: hashedPassword, level: 1 },
+  //     { name: 'Akuntansi', email: 'akuntansi@bpkad.com', pass: hashedPassword, level: 1 },
+  //     { name: 'Aset', email: 'aset@bpkad.com', pass: hashedPassword, level: 1 },
+  //   ],
+  // });
+//   const users = [
+//     { name: 'Admin', email: 'admin@bpkad.com', pass: hashedPassword, level: 1 },
+//     { name: 'Sekretariat', email: 'sekretariat@bpkad.com', pass: hashedPassword, level: 1 },
+//     { name: 'Anggaran', email: 'anggaran@bpkad.com', pass: hashedPassword, level: 1 },
+//     { name: 'Perbendaharaan', email: 'perbendaharaan@bpkad.com', pass: hashedPassword, level: 1 },
+//     { name: 'Akuntansi', email: 'akuntansi@bpkad.com', pass: hashedPassword, level: 1 },
+//     { name: 'Aset', email: 'aset@bpkad.com', pass: hashedPassword, level: 1 },
+// ];
 
-  // await prisma.dinas.create({
-  //   data:{
-  //     nm:"BADAN PENGELOLAAN KEUANGAN DAN ASET DAERAH"
+  // for (const user of users) {
+  //   try {
+  //     await prisma.user.create({ data: user });
+  //   } catch (error) {
+  //     if (error.code === 'P2002') {
+  //       console.log(`⚠️ Email sudah ada: ${user.email}`);
+  //     } else {
+  //       throw error;
+  //     }
   //   }
-  // })
-  // await prisma.bidang.create({
-  //   data:{
-  //     nm:'Anggaran',
-  //     idDin:'93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
-  //     idUser:'9fce9ae9-c88d-42d7-a675-e924118b9b84',
-  //     jabatan:"Kapala Bidang Anggaran",
-  //     jab:"Kabid Anggaran",
-  //     idPimpinan:'9fce9ae9-c88d-42d7-a675-e924118b9b84',
-  //   }
-  // })
-  await prisma.bidang.createMany({
-    data:[
+  // }
+
+
+  const data= [
       {
-        nm:'Akuntansi',
-        idDin:'93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
-        idUser:'3f442bd5-a911-4ac7-b33d-e5ee6657232c',
-        jabatan:"Kapala Bidang Akuntansi",
-        jab:"Kabid Anggaran",
-        idPimpinan:'3f442bd5-a911-4ac7-b33d-e5ee6657232c',
+        nm: 'Akuntansi',
+        idDin: '93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
+        idUser: '3c18a0d5-744b-41df-b38a-fb0ce91a09b3',
+        jabatan: 'Kapala Bidang Akuntansi',
+        jab: 'Kabid Anggaran',
+        idPimpinan: '3c18a0d5-744b-41df-b38a-fb0ce91a09b3',
+      },
+      {
+        nm: 'Sekretariat',
+        idDin: '93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
+        idUser: 'd34f60b0-17e5-4b06-9052-3c2f026dcd25',
+        jabatan: 'Sekretariat',
+        jab: 'Sekretariat',
+        idPimpinan: 'd34f60b0-17e5-4b06-9052-3c2f026dcd25',
+      },
+      {
+        nm: 'Perbendaharaan',
+        idDin: '93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
+        idUser: '7550d4ce-7dd7-4ac7-96c7-3b745df070d2',
+        jabatan: 'Kapala Bidang Perbendaharaan',
+        jab: 'Kabid Perbendaharaan',
+        idPimpinan: '7550d4ce-7dd7-4ac7-96c7-3b745df070d2',
+      },
+      {
+        nm: 'Aset',
+        idDin: '93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
+        idUser: 'a6cc3325-ea59-4709-9f76-e626f7d4f2f4',
+        jabatan: 'Bidang Aset',
+        jab: 'Kabid Aset',
+        idPimpinan: 'a6cc3325-ea59-4709-9f76-e626f7d4f2f4',
       },{
-        nm:'Sekretariat',
-        idDin:'93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
-        idUser:'9fce9ae9-c88d-42d7-a675-e924118b9b84',
-        jabatan:"Sekretariat",
-        jab:"Sekretariat",
-        idPimpinan:'9fce9ae9-c88d-42d7-a675-e924118b9b84',
-      },{
-        nm:'Perbendaharaan',
-        idDin:'93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
-        idUser:'81c52ba6-b5fd-426b-a715-82553eb08f27',
-        jabatan:"Kapala Bidang Perbendaharaan",
-        jab:"Kabid Perbendaharaan",
-        idPimpinan:'81c52ba6-b5fd-426b-a715-82553eb08f27',
-      },{
-        nm:'Aset',
-        idDin:'93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
-        idUser:'b3256555-4ea8-4eeb-9496-15818f229b8e',
-        jabatan:"Bidang Aset",
-        jab:"Kabid Aset",
-        idPimpinan:'b3256555-4ea8-4eeb-9496-15818f229b8e',
+        nm: 'Anggaran',
+        idDin: '93a0d0d7-63f4-44f7-b708-fa05279d7fd0',
+        idUser: '1d4effe2-12c0-40e7-9980-4f3bc969dca0',
+        jabatan: 'Bidang Anggaran',
+        jab: 'Kabid Anggaran',
+        idPimpinan: '1d4effe2-12c0-40e7-9980-4f3bc969dca0',
+      },
+  ];
+  for (const user of data) {
+    try {
+      await prisma.bidang.create({ data: user });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        console.log(`⚠️ Email sudah ada: ${user.email}`);
+      } else {
+        throw error;
       }
-      ]
-  })
+    }
+  }
+
   console.log('✅ User seeded');
 }
 
