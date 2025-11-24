6abbfe8291a36eae1d1d398d27e5992271416c78
hbagus.dev <bagushartiansyah07@gmail.com>
2025-11-24 09:11:30 +0700

    fix,

--- .next/standalone/.next/server/app/app/api/(arsip)/analisis/klasifikasi/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/(arsip)/analisis/klasifikasi/route"] = {"moduleLoading":{"prefix":"","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
